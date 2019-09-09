7db5186241ff57538f75e930529102db2c5da7ca
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-09-09 04:03:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,41 @@
 # Write your code here.
+
+
+
+def take_a_number(katz_deli, name)
+
+  if katz_deli.length == 0 
+katz_deli<<name
+puts "Welcome, #{name}. You are number 1 in line."
+
+elsif katz_deli.length > 0 
+
+array = []
+
+katz_deli.each do  |name, i|
+    name = "#{i+1}. #{name}"
+
+katz_deli<<name
+
+end
+end
+
+
+def line(katz_deli)
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+  
+  elsif katz_deli.length > 0 
+  
+  line_arr = []
+  
+  katz_deli.each_with_index do |name, i|
+    name = "#{i+1}. #{name}"
+      line_arr<<name
+
+end
+  puts "The line is currently: #{line_arr.join(" ")}"
+  end
+end
+
+
