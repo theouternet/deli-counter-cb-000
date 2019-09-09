08f64502fd4734ba490a9237823ba43f6a1a2d40
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-09-09 04:35:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def take_a_number(katz_deli, name)
 
 katz_deli << name
 
-puts "Welcome #{name}. You are number #{katz_deli.length} in line."
+puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 
 end
 
